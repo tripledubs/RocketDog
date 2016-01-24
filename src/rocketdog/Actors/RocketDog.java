77d92c7af166e6d2aa1f0d49d25883932963e255
Hello <dwayne.page@gmail.com>
2016-01-24 00:49:50 -0600

    A compiling program

---
+++ src/rocketdog/Actors/RocketDog.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rocketdog.Actors;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author Dubs
+ */
+public class RocketDog {
+    private Image sprite;
+    public ImageView spriteFrame;
+    public int x;
+    public int y;
+    
+    public RocketDog() {
+        sprite = new Image("/Ugly Dog.png",32,32,true,true);
+        spriteFrame = new ImageView(sprite);
+    }
+
+    public void update() {
+        spriteFrame.setTranslateX(x);
+        spriteFrame.setTranslateY(y);
+    }
+    
+}
---
+++ src/rocketdog/RocketDogGame.java
@@ -0,0 +1,81 @@
+/*
+Copyright 2016
+Noah Sefcik, Richard Dobie, Spencer Harris, Sophia Msaaf
+Dwayne Page, Kody Strickland	
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+package rocketdog;
+
+import rocketdog.Actors.RocketDog;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Dubs
+ */
+public class RocketDogGame extends Application {
+    
+    RocketDog rd;
+    private GamePlayLoop gamePlayLoop;
+    private Image lvl1Image;
+    private ImageView level1ImageView;
+    
+    @Override
+    public void init() {
+        rd = new RocketDog();
+        gamePlayLoop = new GamePlayLoop(this);
+        gamePlayLoop.start();
+        lvl1Image = new Image("/Level 1.png",1000,210,false,false);
+        level1ImageView = new ImageView(lvl1Image);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        root.getChildren().add(level1ImageView);
+        root.getChildren().add(rd.spriteFrame);
+        Scene scene = new Scene(root, 1000, 210);
+        scene.setOnKeyPressed((KeyEvent event) -> {
+            switch (event.getCode()) {
+            }
+        });
+        scene.setOnKeyReleased((KeyEvent event) -> {
+            switch (event.getCode()) {
+                case LEFT:  rd.x-=10; break;
+                case RIGHT: rd.x+=10; break;
+                case UP: rd.y-=10; break;
+                case DOWN: rd.y+=10; break;
+            }  
+        });
+        
+        primaryStage.setTitle("Rocket Dog!!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
