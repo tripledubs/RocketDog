4485950ac3c645453949538e8a3bb823e2f92b24
Spencer Harris <sharris40@uco.edu>
2016-02-19 13:25:10 -0600

    Added Attackable interface. Attackable identifies when an object can be targeted by an attack.

---
+++ src/edu/uco/sdd/rocketdog/model/Attackable.java
@@ -0,0 +1,9 @@
+package edu.uco.sdd.rocketdog.model;
+
+/**
+ * Identifies an object that can be damaged.
+ * @author Spencer Harris
+ */
+public interface Attackable {
+  void damage(double attackStrength);
+}
